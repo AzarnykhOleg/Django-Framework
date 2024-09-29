4e86f2a5cc2e4d455e5009ee7a4c7fa24edee51a
Oleg <olazar61@gmail.com>
2024-09-29 23:57:05 +0300

    HW_2_is_done
    Создайте три модели Django: клиент, товар и заказ.
    
    Клиент может иметь несколько заказов. Заказ может содержать несколько товаров. Товар может входить в несколько заказов.
    
    Поля модели «Клиент»:
    — имя клиента
    — электронная почта клиента
    — номер телефона клиента
    — адрес клиента
    — дата регистрации клиента
    
    Поля модели «Товар»:
    — название товара
    — описание товара
    — цена товара
    — количество товара
    — дата добавления товара
    
    Поля модели «Заказ»:
    — связь с моделью «Клиент», указывает на клиента, сделавшего заказ
    — связь с моделью «Товар», указывает на товары, входящие в заказ
    — общая сумма заказа
    — дата оформления заказа
    
    Допишите несколько функций CRUD для работы с моделями по желанию.

--- online_store/marketplace/models.py
+++ online_store/marketplace/models.py
@@ -1,3 +1,40 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(
+        max_length=12,
+        validators=[
+            RegexValidator(
+                regex=r'^\+?7?\d{9,12}$',
+                message="Номер телефона должен быть введен в формате: '+79999999999'."
+            )
+        ]
+    )
+    # phone_number = models.IntegerField()
+    address = models.TextField()
+    date_of_registration = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Client: {self.name}, email: {self.email}, phone_number: {self.phone_number}'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField()
+    date_of_addition = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Product: {self.title}, описание: {self.description}, цена: {self.price}, количество: {self.quantity}'
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Client, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
