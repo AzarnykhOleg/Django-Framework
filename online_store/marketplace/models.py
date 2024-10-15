ac725ffc9bffbfc5fd348d3797c2de76f0722476
Oleg <olazar61@gmail.com>
2024-10-16 00:29:28 +0300

    HW_3_is_done
    Измените модель продукта, добавьте поле для хранения фотографии продукта.
    
    Создайте форму, которая позволит сохранять фото.

--- online_store/marketplace/models.py
+++ online_store/marketplace/models.py
@@ -30,6 +30,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.IntegerField()
     date_of_addition = models.DateField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/', default='1')
 
     def __str__(self):
         return f'Product: {self.title}, описание: {self.description}, цена: {self.price}, количество: {self.quantity}'
