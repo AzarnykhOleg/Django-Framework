5c53bde115b134de2c5939de484bd7b279e09f62
Oleg <olazar61@gmail.com>
2024-10-22 00:04:34 +0300

    HW_5_is_done

--- online_store/marketplace/admin.py
+++ online_store/marketplace/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
+from .models import Client, Product, Order
+from django.utils.translation import gettext as _
 
-# Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone_number', 'email', 'address']
+    list_display_links = ('name', 'email')
+    ordering = ['name', '-date_of_registration']
+    list_editable = ('phone_number',)
+    list_filter = ['date_of_registration']
+    list_per_page = 10
+    search_fields = ['name']
+    search_help_text = 'Поиск по полю ИМЯ КЛИЕНТА'
+
+
+@admin.action(description="Сбросить количество в ноль")
+def reset_quantity(modeladmin, request, queryset):
+    count = queryset.update(quantity=0)
+    modeladmin.message_user(request, f"Вы обнулили {count} записи(ей).")
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'quantity', 'image']
+    ordering = ['title', '-price']
+    # list_editable = ('quantity',)
+    list_filter = ['price', 'date_of_addition']
+    # list_per_page = 10
+    search_fields = ['description']
+    search_help_text = 'Поиск по полю ОПИСАНИЕ'
+    actions = [reset_quantity]
+
+    readonly_fields = ['date_of_addition']
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['title'],
+            },
+        ),
+        (
+            'Description',
+            {
+                'classes': ['collapse'],
+                'description': 'Товар и его подробное описание',
+                'fields': ['description', 'date_of_addition', 'image'],
+            },
+        ),
+        (
+            'Other',
+            {
+                'fields': ['price', 'quantity'],
+            }
+        ),
+    ]
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'date_ordered', 'total_price']
+    ordering = ['total_price']
+    list_filter = ['total_price', 'customer']
+    list_per_page = 10
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
--- online_store/marketplace/models.py
+++ online_store/marketplace/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 
 class Client(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField()
+    name = models.CharField(max_length=50, verbose_name="Имя клиента")
+    email = models.EmailField(verbose_name="E-mail клиента")
     phone_number = models.CharField(
         max_length=12,
         validators=[
@@ -14,33 +14,45 @@ class Client(models.Model):
                 regex=r'^\+?7?\d{9,12}$',
                 message="Номер телефона должен быть введен в формате: '+79999999999'."
             )
-        ]
+        ],
+        verbose_name="Телефон клиента"
     )
-    # phone_number = models.IntegerField()
-    address = models.TextField()
-    date_of_registration = models.DateField(auto_now_add=True)
+    address = models.TextField(verbose_name="Адрес клиента")
+    date_of_registration = models.DateField(auto_now_add=True, verbose_name="Дата регистрации клиента")
+
+    class Meta:
+        verbose_name = 'Клиенты'
+        verbose_name_plural = 'Клиенты'
 
     def __str__(self):
         return f'Client: {self.name}, email: {self.email}, phone_number: {self.phone_number}'
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=20)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    quantity = models.IntegerField()
-    date_of_addition = models.DateField(auto_now_add=True)
-    image = models.ImageField(upload_to='images/', default='1')
+    title = models.CharField(max_length=20, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание продукта")
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Цена продукта")
+    quantity = models.IntegerField(verbose_name="Количество")
+    date_of_addition = models.DateField(auto_now_add=True, verbose_name="Дата поставки")
+    image = models.ImageField(upload_to='images/', default='1', verbose_name="Изображение")
+
+    class Meta:
+        verbose_name = 'Продукты'
+        verbose_name_plural = 'Продукты'
 
     def __str__(self):
         return f'Product: {self.title}, описание: {self.description}, цена: {self.price}, количество: {self.quantity}'
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Client, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
-    date_ordered = models.DateField(auto_now_add=False)
-    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    customer = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name="Клиент")
+    products = models.ManyToManyField(Product, verbose_name="Продукты")
+    date_ordered = models.DateField(auto_now_add=False, verbose_name="Дата заказа")
+    total_price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Сумма заказа")
+
+    class Meta:
+        verbose_name = 'Заказы'
+        verbose_name_plural = 'Заказы'
 
     def __str__(self):
         return f'Заказ: {self.customer.name}, описание: {self.products}, сумма заказа: {self.total_price}'
